9847fbcb55f516dcced5af9ea57a645a3b9a80a6
Eric W <erice59@users.noreply.github.com>
2022-03-28 10:43:02 -0400

    Initial function declarations to write test suite.

---
+++ program.c
@@ -0,0 +1,33 @@
+#include "program.h"
+
+int main (int argc, char* argv[]){
+  return 0;
+}
+
+float add (float a, float b){
+  return 0.0;
+}
+
+float sub (float a, float b){
+  return 0.0;
+}
+
+float mult (float a, float b){
+  return 0.0;
+}
+
+float div (float a, float b){
+  return 0.0;
+}
+
+float mod (float a, float b){
+  return 0.0;
+}
+
+float num_int (float a, float b){
+  return 0.0;
+}
+
+float num_diff (float a, float b){
+  return 0.0;
+}
---
+++ program.h
@@ -0,0 +1,18 @@
+#ifndef CORECALC
+#define CORECALC
+
+#include <stdio.h>
+#include <strings.h>
+#include <bool.h>
+
+
+float add (float a, float b);
+float sub (float a, float b);
+float div (float a, float b);
+float mod (float a, float b);
+float mult (float a, float b);
+
+float num_int (float a, float b);
+float num_diff (float a, float b);
+
+#endif
